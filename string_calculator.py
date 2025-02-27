d74dca9bb3d1bd5c31eac744cf1427cec674c0f3
Subodh Singh <subodhdevpatel@gmail.com>
2025-02-27 18:26:28 +0530

    implement: custom delimiter

--- string_calculator.py
+++ string_calculator.py
@@ -4,5 +4,12 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
-        numbers = re.split(r"[\n,]", numbers)
+
+        delimiter = r"[\n,]"
+        if numbers.startswith("//"):
+            parts = numbers.split("\n", 1)
+            delimiter = re.escape(parts[0][2:])
+            numbers = parts[1]
+
+        numbers = re.split(delimiter, numbers)
         return sum(int(n) for n in numbers)
