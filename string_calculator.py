16c1d8bad5ca1e547f9a5b10842ee9c66b1e3a52
Subodh Singh <subodhdevpatel@gmail.com>
2025-02-27 18:07:30 +0530

    implement: multiple numbers

--- string_calculator.py
+++ string_calculator.py
@@ -2,4 +2,4 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
-        return int(numbers) if numbers.isdigit() else 0
+        return sum(int(n) for n in numbers.split(","))
