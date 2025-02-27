97c4456b1a2b15bfbe226090f5a0ca3376b0522a
Subodh Singh <subodhdevpatel@gmail.com>
2025-02-27 17:51:25 +0530

    test: none returns zero

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -5,3 +5,8 @@ from string_calculator import StringCalculator
 def test_empty_string_returns_zero():
     calc = StringCalculator()
     assert calc.add("") == 0
+
+
+def test_none_returns_zero():
+    calc = StringCalculator()
+    assert calc.add(None) == 0
