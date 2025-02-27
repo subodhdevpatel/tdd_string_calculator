894d8ebeb4ba4b7ea6141eb6670c8bd0251449bd
Subodh Singh <subodhdevpatel@gmail.com>
2025-02-27 17:54:50 +0530

    test: single number returns same number

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -10,3 +10,9 @@ def test_empty_string_returns_zero():
 def test_none_returns_zero():
     calc = StringCalculator()
     assert calc.add(None) == 0
+
+
+def test_single_number_returns_same_number():
+    calc = StringCalculator()
+    assert calc.add("5") == 5
+    assert calc.add("15") == 15
